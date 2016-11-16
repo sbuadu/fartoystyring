e1c93ed0338f24aa09f71077e4e4f47e6f97a798
Stephanie <steffyquayson@hotmail.com>
2016-11-16 19:16:25 +0100

    Changed the kontroller gain definition

--- task 1.8/run_task_1_8.m
+++ task 1.8/run_task_1_8.m
@@ -53,15 +53,15 @@ deg2rad = pi/180;
 rad2deg = 180/pi;
 
 
-
 %Declaring the gains for the heading controller
 K_ppsi = 3; %Proportional gain
 K_ipsi = 3; %Integral gain
 K_dpsi = 2; %Derivative gain
 
 %Declaring the gains for the speed controller
-K_pu = 18; %Proportional gain
-K_iu = 9^2; %Integral gain
+lambda = 10; 
+K_pu = 2*lambda; %Proportional gain
+K_iu = lambda*2; %Integral gain
 K_du = 0; %Derivative gain
         
         
