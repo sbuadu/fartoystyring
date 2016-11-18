6243498eba53afa5a555af2b8902a3053b68e499
Stephanie <steffyquayson@hotmail.com>
2016-11-18 17:47:11 +0100

    Cleaned up the plotting.m

--- task 2.7/plotting.m
+++ task 2.7/plotting.m
@@ -1,21 +1,3 @@
-
-% %plot(P(:,2), P(:,1));
-% axis equal;
-% grid on;
-% hold on;
-% 
-% x = WP(1,:);
-% y = WP(2,:);
-% 
-% % Plot waypoints and desired path
-% %plot(y,x,'o',y,x);
-% 
-% title('Path generation');
-% xlabel('East [y]');
-% ylabel('North [x]');
-% legend = legend('MS Fartoystyring', 'Waypoints','Desired path','Location','SouthEast');
-% set(legend,'FontSize',12);
-
 %ploting closed loop surge speed behaviour
 figure 
 hold on
