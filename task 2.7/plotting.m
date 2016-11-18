949151d1df13d350e269a68667fca50d4a31bc77
Stephanie <steffyquayson@hotmail.com>
2016-11-18 18:30:02 +0100

    Made the plot of angles

--- task 2.7/plotting.m
+++ task 2.7/plotting.m
@@ -41,3 +41,17 @@ title('Closed loop shaft speed behavoiur', 'Interpreter', 'latex');
 legend('n_c', 'saturation limit', 'saturation limit'); 
 xlabel('$Time [s]$','Interpreter', 'latex');
 ylabel('$Shaft speed [rad/s]$','Interpreter', 'latex');
+
+
+%generating plot to all angles
+figure
+hold on 
+grid on 
+plot(beta*rad2deg); 
+plot(x*rad2deg); 
+plot(x_d*rad2deg, '--'); 
+plot(psi*rad2deg); 
+title('Angle comparison', 'Interpreter', 'latex'); 
+legend('\beta', '\chi', '\chi_d', '\psi', 'Interpreter', 'latex'); 
+xlabel('s', 'Interpreter', 'latex'); 
+ylabel( '[deg]', 'Interpreter', 'latex'); 
